6a29f28d96302d78f12af8bd730786095ab242a1
Leslie Castellanos <lesliekate@gmail.com>
2017-01-09 19:54:37 -0800

    Notes for adding SelectionsContainer

---
+++ app/containers/SelectionsContainer.js
@@ -0,0 +1,21 @@
+// manage category state and on change update dependent dropdown
+
+// var React = require('react');
+// var Indices = require('../components/Indices')
+// var Selections = require('./Selections');
+
+// var SelectionsContainer = React.createClass ({
+// getInitialState: function () {
+//  set state indices = Indices;
+//  set state categories = Object.keys(Indices).map((index) =>
+//   index
+//  );
+//   return state;
+// },
+// })
+// handleChangeCategory: function(evt) {
+//   this.setState({classes: Object.keys(state.indices[evt.target.value])});
+// },
+// render: function() {
+// <Selections categories=this.state.categories classes=this.state.classes  />
+// }
