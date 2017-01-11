56755beee7036c611671f2e9095a8096dc5d9b4b
Leslie Castellanos <lesliekate@gmail.com>
2017-01-11 09:10:49 -0800

    updates to selectionscontainer notes

--- app/containers/SelectionsContainer.js
+++ app/containers/SelectionsContainer.js
@@ -1,21 +1,24 @@
-// manage category state and on change update dependent dropdown
-
+// // manage category state and on change update dependent dropdown
+//
 // var React = require('react');
 // var Indices = require('../components/Indices')
 // var Selections = require('./Selections');
-
+//
 // var SelectionsContainer = React.createClass ({
-// getInitialState: function () {
-//  set state indices = Indices;
-//  set state categories = Object.keys(Indices).map((index) =>
-//   index
-//  );
-//   return state;
-// },
-// })
-// handleChangeCategory: function(evt) {
-//   this.setState({classes: Object.keys(state.indices[evt.target.value])});
-// },
-// render: function() {
-// <Selections categories=this.state.categories classes=this.state.classes  />
-// }
+//   getInitialState: function () {
+//     return {indices: Indices, categories = Object.keys(Indices).map((index) => index)});
+//     return state}
+//   })
+//   handleChangeCategory: function(evt) {
+//     this.setState({classes: Object.keys(state.indices[evt.target.value])});
+//   },
+//   render: function() {
+//     return(
+//       <Selections
+//       categories=this.state.categories
+//       classes=this.state.classes
+//       onChangeCategory={this.handleChangeCategory}
+//       />
+//     )
+//   }
+// });
