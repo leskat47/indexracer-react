c2c04982493a400eac56fccfd5ff4277a6d3d36a
Leslie Castellanos <lesliekate@gmail.com>
2017-01-16 09:02:53 -0800

    Make placeholder on classes select

--- app/components/Selections.js
+++ app/components/Selections.js
@@ -7,14 +7,16 @@ function Selections (props) {
       <form>
         <h2>{props.pronoun} SCCA Class Category:</h2>
         <select onChange={props.onChangeCategory}>
+          <option value="" disabled selected>Select Category</option>
           {props.cats.map((choice) => <option key={choice} value={choice}>{choice}</option>)}
         </select>
         <h2>{props.pronoun}  SCCA Class:</h2>
         <select onChange={props.onChangeClass}>
+          <option value="" disabled selected>Select Class</option>
           {props.classes.map((choice) => <option key={choice} value={choice}>{choice}</option>)}
         </select>
         <h2>Enter {props.pronoun} Time:</h2>
-        <input type="text" value={props.time} onChange={props.showResults} />
+        <input type="text" value={props.time} onChange={props.showResults} disabled={ props.class == "" }/>
       </form>
       <button type="button">Clear</button>
     </div>
